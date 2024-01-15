df108714111a1c1c73b4e1bf1676c8c0173098bf
Jodieadver <lisijia1028111@gmail.com>
2024-01-15 01:15:23 +0100

    onboarding
    finished landing_page, log_in page, sign_up page, and their redirections

---
+++ web/front-end/app.js
@@ -0,0 +1,10 @@
+function redirectToLogIn() {
+    // Redirect to log_in.html
+    window.location.href = "log_in.html";
+}
+
+function redirectToSignUp() {
+    // Redirect to sign_up.html
+    window.location.href = "sign_up.html";
+}
+
