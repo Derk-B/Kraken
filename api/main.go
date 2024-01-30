d9bec1bd2151ef377360fcb7b3cb770dad93a664
Derk Blom <d.blom2@student.vu.nl>
2024-01-30 12:32:25 +0100

    Api now loads env variables from .env file

--- api/main.go
+++ api/main.go
@@ -1,31 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"kraken/api-server/models"
 	"kraken/api-server/server"
-	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	// Get env variables for connection string
-	//envFile, err := godotenv.Read("../.env")
-	//
-	//if err != nil {
-	//	fmt.Println("Could not load env file")
-	//}
+	envFile, err := godotenv.Read("../.env")
+
+	if err != nil {
+		fmt.Println("Could not load env file")
+	}
 
 	// Create database connection
-	//PSQL_PORT := envFile["PSQL_PORT"]
-	//PSQL_USERNAME := envFile["PSQL_USERNAME"]
-	//PSQL_PASSWORD := envFile["PSQL_PASSWORD"]
-	//PSQL_DATABASE := envFile["PSQL_DATABASE"]
-	//PSQL_HOSTNAME := envFile["PSQL_HOSTNAME"]
-
-	PSQL_PORT := os.Getenv("PSQL_PORT")
-	PSQL_USERNAME := os.Getenv("PSQL_USERNAME")
-	PSQL_PASSWORD := os.Getenv("PSQL_PASSWORD")
-	PSQL_DATABASE := os.Getenv("PSQL_DATABASE")
-	PSQL_HOSTNAME := os.Getenv("PSQL_HOSTNAME")
+	PSQL_PORT := envFile["PSQL_PORT"]
+	PSQL_USERNAME := envFile["PSQL_USERNAME"]
+	PSQL_PASSWORD := envFile["PSQL_PASSWORD"]
+	PSQL_DATABASE := envFile["PSQL_DATABASE"]
+	PSQL_HOSTNAME := envFile["PSQL_HOSTNAME"]
 
 	models.Connect(PSQL_USERNAME, PSQL_PASSWORD, PSQL_PORT, PSQL_HOSTNAME, PSQL_DATABASE)
 	s := server.New()
