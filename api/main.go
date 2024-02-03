7677f3cf284304a18500d82c800f72d3f6ff79d3
caesarw <caesar.wu@outlook.com>
2024-02-03 11:35:50 +0000

    fix(api): incorrect env for database name

--- api/main.go
+++ api/main.go
@@ -13,19 +13,17 @@ func main() {
 	//if err != nil {
 	//	fmt.Println("Could not load env file")
 	//}
-
-	// Create database connection
 	//PSQL_PORT := envFile["PSQL_PORT"]
 	//PSQL_USERNAME := envFile["PSQL_USERNAME"]
 	//PSQL_PASSWORD := envFile["PSQL_PASSWORD"]
 	//PSQL_DATABASE := envFile["PSQL_DATABASE"]
 	//PSQL_HOSTNAME := envFile["PSQL_HOSTNAME"]
 
+	PSQL_HOSTNAME := os.Getenv("POSTGRES_HOSTNAME")
 	PSQL_PORT := os.Getenv("POSTGRES_PORT")
 	PSQL_USERNAME := os.Getenv("POSTGRES_USERNAME")
 	PSQL_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
-	PSQL_DATABASE := os.Getenv("POSTGRES_DATABASE")
-	PSQL_HOSTNAME := os.Getenv("POSTGRES_HOSTNAME")
+	PSQL_DATABASE := os.Getenv("POSTGRES_DB")
 
 	models.Connect(PSQL_USERNAME, PSQL_PASSWORD, PSQL_PORT, PSQL_HOSTNAME, PSQL_DATABASE)
 	s := server.New()
